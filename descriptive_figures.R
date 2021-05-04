ce0ab8a1ba267edd2ad4ad4592d36df3c34702cc
Colin Gross <grosscol@gmail.com>
2021-05-04 01:19:19 -0400

    Further refine outcomes plot   Avoid name confusion as "output" plot. Begin ring plot experimentation.

--- descriptive_figures.R
+++ descriptive_figures.R
@@ -53,7 +53,7 @@ intervention_counts <- df %>%
 # Turn off "helpful" message from summarize about experimental .grouping feature
 options('dplyr.summarise.inform'=FALSE)
 
-out_plot_data <- df %>% 
+outc_plot_data <- df %>% 
   select(intervention, outcome)  %>%
   filter(!is.na(outcome)) %>%
   mutate(outcome = recode(outcome, `1`= 'Fetal Demise',  `2` = 'NND',  
@@ -63,18 +63,52 @@ out_plot_data <- df %>%
   summarize(numerator = n()) %>%
   left_join(x=., y=intervention_counts, by="intervention") %>%
   mutate(rate = numerator/denominator,
-         numer_label = sprintf("%% %2.0f",rate*100)
+         rate_label = sprintf("%2.0f%%",rate*100)
          )
-out_plot_data 
+outc_plot_data 
 
-ggplot(data=out_plot_data, aes(x=intervention, y=rate, group=outcome)) +
+outc_plot <- ggplot(data=outc_plot_data, aes(x=intervention, y=rate, group=outcome)) +
   geom_bar(aes(fill=outcome), stat="identity") +
   scale_fill_brewer(palette = "PRGn") +
   geom_text(size = 4,
-            aes(label = numer_label),
+            aes(label = rate_label),
             color='black',
             fontface='bold',
             position = position_stack(vjust = 0.5),
-            show.legend = F)
+            show.legend = F) +
+  ylab("Rate") +
+  xlab("Intervention") +
+  theme_classic() +
+  theme(axis.text.y = element_blank()) 
+outc_plot
 
+#########################
+# Ring Plots Experiment #
+#########################
+
+# Begin with outcomes data from above
+ring_plot_data <- outc_plot_data %>% 
+  filter(intervention == "Laser") %>%
+  mutate(position=9,
+         fract_label = sprintf("%% %i/%i",numerator, denominator),
+         denom_label = sprintf("%i Cases", denominator))
+
+ring_laser <- ggplot(data=ring_plot_data, aes(x=position, y=rate, group=outcome)) +
+  geom_bar(aes(fill=outcome), color='black', width=2, stat="identity") +
+  geom_text(aes(label=rate_label), position = position_stack(vjust=.5), size = 8) +
+  geom_text(aes(label=intervention), y=1, x=4, size = 32) +
+  geom_text(aes(label=denom_label), y=1, x=1.5, size = 16) +
+  scale_x_continuous(limits=c(0,10)) +
+  coord_polar(theta="y", direction=1) +
+  scale_fill_brewer(palette = "PRGn", name="Outcome") +
+  theme_classic() +
+  theme(axis.line=element_blank(),    axis.text.x=element_blank(),
+        axis.text.y=element_blank(),  axis.ticks=element_blank(),
+        axis.title.x=element_blank(), axis.title.y=element_blank(),
+        panel.background=element_blank(), panel.border=element_blank(),
+        panel.grid.major=element_blank(), panel.grid.minor=element_blank(),
+        plot.margin = unit(c(-.1,-.1,-.1,-.1),"npc"),
+        legend.position = c(.5,.4),
+        legend.text = element_text(size=20),
+        legend.title = element_text(size=22, hjust=.45))
 
