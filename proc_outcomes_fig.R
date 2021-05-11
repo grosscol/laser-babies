573d99f7dc264183e42026c5676bf4fed0f3e2ae
Colin Gross <grosscol@gmail.com>
2021-05-10 20:18:18 -0400

    Aggregate outcomes into single figure

--- proc_outcomes_fig.R
+++ proc_outcomes_fig.R
@@ -2,16 +2,29 @@ library("readr")
 library("dplyr")
 library("ggplot2")
 library("tidyr")
+library("stringr")
 
 df <- read_csv('SCT_percutaneous_interventions.csv')
 
-##############
-# Ring Plots # 
-##############
+#######################
+# Base Ring Plot Data # 
+#######################
 
+# Like to_title, but doesn't lower the rest of the chars
+upcase_first <- function(x){
+  
+}
+
+intervention_counts <- df %>%
+  filter(!is.na(outcome)) %>%
+  select(intervention) %>%
+  group_by(intervention) %>%
+  summarize(denominator = n())
+
+# Base data will need to be filtered by intervention.
 ring_plot_data <- df %>% 
   select(intervention, outcome)  %>%
-  filter(!is.na(outcome) & intervention == "Laser") %>%
+  filter(!is.na(outcome)) %>%
   mutate(outcome = recode(outcome, `1`= 'Fetal Demise',  `2` = 'NND',  
                           `3` = 'Discharge', `4` = 'Abortion', 
                           .default=as.character(outcome))) %>%
@@ -24,11 +37,18 @@ ring_plot_data <- df %>%
          fract_label = sprintf("%% %i/%i",numerator, denominator),
          denom_label = sprintf("%i Cases", denominator))
 
-ring_laser_plot <- ggplot(data=ring_plot_data, aes(x=position, y=rate, group=outcome)) +
+# Text sizes
+sz_percents <- 4
+sz_center_sub <- 6
+sz_legend <- 20
+sz_legend_title <- 22
+sz_facet = 12
+
+multi_plot <- ggplot(data=ring_plot_data, aes(x=position, y=rate, group=outcome)) +
   geom_bar(aes(fill=outcome), color='black', width=2, stat="identity") +
-  geom_text(aes(label=rate_label), position = position_stack(vjust=.5), size = 8) +
-  geom_text(aes(label=intervention), y=1, x=4, size = 32) +
-  geom_text(aes(label=denom_label), y=1, x=1.5, size = 16) +
+  geom_text(aes(label=rate_label, x=position-2.5), 
+            position = position_stack(vjust=.5), size = sz_percents) +
+  geom_text(aes(label=denom_label), y=1, x=0.5, size = sz_center_sub) +
   scale_x_continuous(limits=c(0,10)) +
   coord_polar(theta="y", direction=1) +
   scale_fill_brewer(palette = "PRGn", name="Outcome") +
@@ -38,11 +58,13 @@ ring_laser_plot <- ggplot(data=ring_plot_data, aes(x=position, y=rate, group=out
         axis.title.x=element_blank(), axis.title.y=element_blank(),
         panel.background=element_blank(), panel.border=element_blank(),
         panel.grid.major=element_blank(), panel.grid.minor=element_blank(),
-        plot.margin = unit(c(-.1,-.1,-.1,-.1),"npc"),
-        legend.position = c(.5,.4),
-        legend.text = element_text(size=20),
-        legend.title = element_text(size=22, hjust=.45))
-ring_laser_plot
+        legend.position = c(.8,.35),
+        legend.text = element_text(size=sz_legend),
+        legend.title = element_text(size=sz_legend_title, hjust=.45),
+        strip.text.x = element_text(size=sz_facet),
+        plot.margin = margin(.2,0,.2,0, "in")) +
+  facet_wrap(facets=vars(intervention), nrow=2, ncol=2)
 
 # Save plot
-ggsave("outcome_by_proc_fig.png", plot=ring_laser_plot, device="png")
+ggsave("outcome_by_proc_fig.png", plot=multi_plot, 
+       width = 8, height = 7, units='in', device="png")
